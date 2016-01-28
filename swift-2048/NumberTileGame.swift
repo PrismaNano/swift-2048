093b89eed8426c84a62234c2d839c9a9065ea01c
Daniel Mathews <dannyl.mathews@gmail.com>
2016-01-27 18:47:37 -0800

    working with swift 2.0

--- swift-2048/NumberTileGame.swift
+++ swift-2048/NumberTileGame.swift
@@ -44,7 +44,7 @@ class NumberTileGameViewController : UIViewController, GameModelProtocol {
     setupSwipeControls()
   }
 
-  required init(coder aDecoder: NSCoder) {
+  required init?(coder aDecoder: NSCoder) {
     fatalError("NSCoding not supported")
   }
 
@@ -102,7 +102,7 @@ class NumberTileGameViewController : UIViewController, GameModelProtocol {
       assert(views.count > 0)
       assert(order >= 0 && order < views.count)
       let viewHeight = views[order].bounds.size.height
-      let totalHeight = CGFloat(views.count - 1)*viewPadding + views.map({ $0.bounds.size.height }).reduce(verticalViewOffset, { $0 + $1 })
+      let totalHeight = CGFloat(views.count - 1)*viewPadding + views.map({ $0.bounds.size.height }).reduce(verticalViewOffset, combine: { $0 + $1 })
       let viewsTop = 0.5*(vcHeight - totalHeight) >= 0 ? 0.5*(vcHeight - totalHeight) : 0
 
       // Not sure how to slice an array yet
@@ -136,12 +136,12 @@ class NumberTileGameViewController : UIViewController, GameModelProtocol {
 
     var f = scoreView.frame
     f.origin.x = xPositionToCenterView(scoreView)
-    f.origin.y = yPositionForViewAtPosition(0, views)
+    f.origin.y = yPositionForViewAtPosition(0, views: views)
     scoreView.frame = f
 
     f = gameboard.frame
     f.origin.x = xPositionToCenterView(gameboard)
-    f.origin.y = yPositionForViewAtPosition(1, views)
+    f.origin.y = yPositionForViewAtPosition(1, views: views)
     gameboard.frame = f
 
 
