06b3909d948b0c5b09a913e4718944f862035494
Sam Meech-Ward <sam@meech-ward.com>
2016-10-26 09:57:36 -0700

    Migrate code to Swift 3

--- swift-2048Tests/swift_2048Tests.swift
+++ swift-2048Tests/swift_2048Tests.swift
@@ -27,7 +27,7 @@ class swift_2048Tests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock() {
+        self.measure() {
             // Put the code you want to measure the time of here.
         }
     }
